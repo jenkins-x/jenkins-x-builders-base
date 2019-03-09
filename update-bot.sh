227493660bf44aced1fe6b54c071debf5ea99634
rawlingsj <rawlingsj80@gmail.com>
2019-03-09 08:59:13 +0000

    initial checkin of moving base images from builder repo

---
+++ update-bot.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+updatebot push-version --kind docker \
+    jenkinsxio/jenkins-x-builders ${VERSION}
+updatebot push-regex -r "builderTag: (.*)" -v ${VERSION} jx-build-templates/values.yaml
+updatebot push-regex -r "\s+tag: (.*)" -v ${VERSION} --previous-line "\s+repository: jenkinsxio/builder-go" values.yaml
+updatebot push-regex -r "\s+tag: (.*)" -v ${VERSION} --previous-line "\s+repository: jenkinsxio/builder-maven" prow/values.yaml
