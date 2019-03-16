7dfd3e52dbb71ef3ec539fea794e06fbac960950
JenkinsXBot <jenkins-x@googlegroups.com>
2019-03-16 10:57:25 +0000

    fix: updatebot reversion replacement

--- update-bot.sh
+++ update-bot.sh
@@ -5,6 +5,8 @@ set -o nounset
 set -o pipefail
 
 updatebot push-version --kind docker \
+    gcr.io/jenkinsxio/builder-base ${VERSION}
+updatebot push-version --kind docker \
     jenkinsxio/jenkins-x-builders ${VERSION}
 updatebot push-regex -r "builderTag: (.*)" -v ${VERSION} jx-build-templates/values.yaml
 updatebot push-regex -r "\s+tag: (.*)" -v ${VERSION} --previous-line "\s+repository: jenkinsxio/builder-go" values.yaml
