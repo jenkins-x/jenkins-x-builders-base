a01da82577424a59b07b9f278fd4850675d71c9a
JenkinsXBot <jenkins-x@googlegroups.com>
2019-03-16 14:12:36 +0000

    fix: updatebot on jenkins-x-builders

--- update-bot.sh
+++ update-bot.sh
@@ -4,10 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-updatebot push-version --kind docker \
-    gcr.io/jenkinsxio/builder-base ${VERSION}
-updatebot push-version --kind docker \
-    jenkinsxio/jenkins-x-builders ${VERSION}
-updatebot push-regex -r "builderTag: (.*)" -v ${VERSION} jx-build-templates/values.yaml
-updatebot push-regex -r "\s+tag: (.*)" -v ${VERSION} --previous-line "\s+repository: jenkinsxio/builder-go" values.yaml
-updatebot push-regex -r "\s+tag: (.*)" -v ${VERSION} --previous-line "\s+repository: jenkinsxio/builder-maven" prow/values.yaml
+updatebot push-regex -r "FROM gcr.io/jenkinsxio/builder-base:(.*)" -v ${VERSION} "*/Dockerfile"
+updatebot push-regex -r "\s+- --image=gcr.io/jenkinsxio/builder-base:(.*)" -v ${VERSION} "jenkins-x.yml"
