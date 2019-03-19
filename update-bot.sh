f59ab09550538089c0414b23e06cdaf53554b178
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-03-19 16:26:12 +0000

    feat: pushing ruby/swift images to the correct base image locations

--- update-bot.sh
+++ update-bot.sh
@@ -5,4 +5,8 @@ set -o nounset
 set -o pipefail
 
 updatebot push-regex -r "FROM gcr.io/jenkinsxio/builder-base:(.*)" -v ${VERSION} "*/Dockerfile"
+updatebot push-regex -r "FROM gcr.io/jenkinsxio/builder-rubybase:(.*)" -v ${VERSION} "*/Dockerfile"
+updatebot push-regex -r "FROM gcr.io/jenkinsxio/builder-swiftbase:(.*)" -v ${VERSION} "*/Dockerfile"
 updatebot push-regex -r "\s+- --image=gcr.io/jenkinsxio/builder-base:(.*)" -v ${VERSION} "jenkins-x.yml"
+updatebot push-regex -r "\s+- --image=gcr.io/jenkinsxio/builder-rubybase:(.*)" -v ${VERSION} "jenkins-x.yml"
+updatebot push-regex -r "\s+- --image=gcr.io/jenkinsxio/builder-swiftbase:(.*)" -v ${VERSION} "jenkins-x.yml"
