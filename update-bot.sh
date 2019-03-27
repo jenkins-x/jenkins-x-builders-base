cbb71a4563437792a6bc7cede4a49e5ae9618032
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-03-27 10:46:50 +0000

    fix: stop variable expansion for updatebot command

--- update-bot.sh
+++ update-bot.sh
@@ -3,13 +3,14 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -f
 
 updatebot push-regex -r \
-	"FROM\sgcr.io/jenkinsxio/builder-base:(.*)" \
-	"FROM\sgcr.io/jenkinsxio/builder-rubybase:(.*)" \
-	"FROM\sgcr.io/jenkinsxio/builder-swiftbase:(.*)" \
-	"\s+-\s--image=gcr.io/jenkinsxio/builder-base:(.*)" \
-	"\s+-\s--image=gcr.io/jenkinsxio/builder-rubybase:(.*)" \
-	"\s+-\s--image=gcr.io/jenkinsxio/builder-swiftbase:(.*)" \
+	'FROM\sgcr.io/jenkinsxio/builder-base:(.*)' \
+	'FROM\sgcr.io/jenkinsxio/builder-rubybase:(.*)' \
+	'FROM\sgcr.io/jenkinsxio/builder-swiftbase:(.*)' \
+	'\s+-\s--image=gcr.io/jenkinsxio/builder-base:(.*)' \
+	'\s+-\s--image=gcr.io/jenkinsxio/builder-rubybase:(.*)' \
+	'\s+-\s--image=gcr.io/jenkinsxio/builder-swiftbase:(.*)' \
 	-v ${VERSION} \
-	 'Dockerfile' 'jenkins-x.yml'
+	 \*/Dockerfile jenkins-x.yml
