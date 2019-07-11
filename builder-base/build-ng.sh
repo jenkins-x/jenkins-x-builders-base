feaea45194cc62126f058dd893d13cba061a3f5d
Andrew Bayer <andrew.bayer@gmail.com>
2019-07-11 11:47:09 -0400

    chore: add builder-*base images for machine learning and ml gpu
    Downstream of https://github.com/jenkins-x/jenkins-x-builders-base-image/pull/18
    
    part of https://github.com/jenkins-x/jx/issues/4670
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- builder-base/build-ng.sh
+++ builder-base/build-ng.sh
@@ -21,3 +21,5 @@ function build_image {
 
 build_image "ruby" "rubybase"
 build_image "swift" "swiftbase"
+build_image "machinelearning" "machinelearningbase"
+build_image "machinelearninggpu" "machinelearninggpubase"
--- update-bot.sh
+++ update-bot.sh
@@ -9,8 +9,12 @@ updatebot push-regex -r \
 	'FROM\sgcr.io/jenkinsxio/builder-base:(.*)' \
 	'FROM\sgcr.io/jenkinsxio/builder-rubybase:(.*)' \
 	'FROM\sgcr.io/jenkinsxio/builder-swiftbase:(.*)' \
+	'FROM\sgcr.io/jenkinsxio/builder-machinelearningbase:(.*)' \
+	'FROM\sgcr.io/jenkinsxio/builder-machinelearninggpubase:(.*)' \
 	'\s+-\s--image=gcr.io/jenkinsxio/builder-base:(.*)' \
 	'\s+-\s--image=gcr.io/jenkinsxio/builder-rubybase:(.*)' \
 	'\s+-\s--image=gcr.io/jenkinsxio/builder-swiftbase:(.*)' \
+	'\s+-\s--image=gcr.io/jenkinsxio/builder-machinelearningbase:(.*)' \
+	'\s+-\s--image=gcr.io/jenkinsxio/builder-machinelearninggpubase:(.*)' \
 	-v ${VERSION} \
 	 \*/Dockerfile jenkins-x.yml
