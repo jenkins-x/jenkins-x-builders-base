64b68081380a17be21a7111b5f3cebf8df967968
Andrew Bayer <andrew.bayer@gmail.com>
2019-09-28 10:14:59 -0400

    fix: Unbreak updatebot
    My attempt to simplify the calls did not work. I think because we're
    using a too-old image here, but I'm not gonna worry about it for now
    and just switch back to the multiple calls.
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- update-bot.sh
+++ update-bot.sh
@@ -4,19 +4,37 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+jx step create pr docker \
+    --name gcr.io/jenkinsxio/builder-base \
+    --version ${VERSION} \
+    --repo https://github.com/jenkins-x/jenkins-x-builders.git \
+    --repo https://github.com/jenkins-x/jenkins-x-builders-ml.git
+
+jx step create pr docker \
+    --name gcr.io/jenkinsxio/builder-rubybase \
+    --version ${VERSION} \
+    --repo https://github.com/jenkins-x/jenkins-x-builders.git
+
+jx step create pr docker \
+    --name gcr.io/jenkinsxio/builder-swiftbase \
+    --version ${VERSION} \
+    --repo https://github.com/jenkins-x/jenkins-x-builders.git
+
 jx step create pr regex \
     --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-base:(.*)' \
-    --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-rubybase:(.*)' \
-    --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-swiftbase:(.*)' \
     --version ${VERSION} \
     --files jenkins-x.yml \
     --repo https://github.com/jenkins-x/jenkins-x-builders.git \
     --repo https://github.com/jenkins-x/jenkins-x-builders-ml.git
 
-jx step create pr docker \
-    --name gcr.io/jenkinsxio/builder-base \
-    --name gcr.io/jenkinsxio/builder-rubybase \
-    --name gcr.io/jenkinsxio/builder-swiftbase \
+jx step create pr regex \
+    --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-rubybase:(.*)' \
     --version ${VERSION} \
-    --repo https://github.com/jenkins-x/jenkins-x-builders.git \
-    --repo https://github.com/jenkins-x/jenkins-x-builders-ml.git
+    --files jenkins-x.yml \
+    --repo https://github.com/jenkins-x/jenkins-x-builders.git
+
+jx step create pr regex \
+    --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-swiftbase:(.*)' \
+    --version ${VERSION} \
+    --files jenkins-x.yml \
+    --repo https://github.com/jenkins-x/jenkins-x-builders.git
