22c0e89016b17a6866c7f5599639e05beed140fe
Andrew Bayer <andrew.bayer@gmail.com>
2019-09-30 12:28:29 -0400

    fix: Update builder image, reconsolidate create pr calls
    It still will trigger three runs, but at least it won't trigger 11.
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- update-bot.sh
+++ update-bot.sh
@@ -6,35 +6,17 @@ set -o pipefail
 
 jx step create pr docker \
     --name gcr.io/jenkinsxio/builder-base \
+    --name gcr.io/jenkinsxio/builder-rubybase \
+    --name gcr.io/jenkinsxio/builder-swiftbase \
     --version ${VERSION} \
     --repo https://github.com/jenkins-x/jenkins-x-builders.git \
     --repo https://github.com/jenkins-x/jenkins-x-builders-ml.git
-
-jx step create pr docker \
-    --name gcr.io/jenkinsxio/builder-rubybase \
-    --version ${VERSION} \
-    --repo https://github.com/jenkins-x/jenkins-x-builders.git
-
-jx step create pr docker \
-    --name gcr.io/jenkinsxio/builder-swiftbase \
-    --version ${VERSION} \
-    --repo https://github.com/jenkins-x/jenkins-x-builders.git
 
 jx step create pr regex \
     --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-base:(.*)' \
+    --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-rubybase:(.*)' \
+    --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-swiftbase:(.*)' \
     --version ${VERSION} \
     --files jenkins-x.yml \
     --repo https://github.com/jenkins-x/jenkins-x-builders.git \
     --repo https://github.com/jenkins-x/jenkins-x-builders-ml.git
-
-jx step create pr regex \
-    --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-rubybase:(.*)' \
-    --version ${VERSION} \
-    --files jenkins-x.yml \
-    --repo https://github.com/jenkins-x/jenkins-x-builders.git
-
-jx step create pr regex \
-    --regex '\s+-\s--image=gcr.io/jenkinsxio/builder-swiftbase:(.*)' \
-    --version ${VERSION} \
-    --files jenkins-x.yml \
-    --repo https://github.com/jenkins-x/jenkins-x-builders.git
